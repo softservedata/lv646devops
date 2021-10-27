8ad46a07ce99ea31f3fa7a59edb27877dca88439
Softservedata <garasym@gmail.com>
2021-10-27 12:42:31 +0300

    OMS Application

---
+++ scripts/MySQL1.bat
@@ -0,0 +1,10 @@
+rem Goto to C:\Program Files\MySQL\MySQL Server 5.7\bin
+rem mysql -p -u root lv459oms < MySQL.sql
+
+rem mysql -p -u root lv459oms < MySQL.sql
+
+rem mysql -h 192.168.153.128 -u lv542  --password="Pa$$lv542" < MySQL.sql
+
+
+mysql -h 192.168.153.128 -u lv542  --password="Pa$$lv542" < MySQLupdate.sql.txt
+
